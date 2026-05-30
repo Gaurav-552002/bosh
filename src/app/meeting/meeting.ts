77a9da3ba15b8547ac88f52011a3296721cfe31a
Gaurav <gmkulkarni7499@gmail.com>
2026-05-30 16:24:37 +0530

    meeting page update

--- src/app/meeting/meeting.ts
+++ src/app/meeting/meeting.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-meeting',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './meeting.html',
   styleUrl: './meeting.css',
 })
-export class Meeting {
 
+export class Meeting {
+    searchText = '';
+
+  appointments = [
+    {
+      visitorName: '-',
+      visitorFirm: '-',
+      visitDate: '-',
+      visitTime: '-'
+    }
+  ];
+
+  filteredAppointments() {
+    return this.appointments.filter(item =>
+      item.visitorName
+        .toLowerCase()
+        .includes(this.searchText.toLowerCase())
+    );
+  }
+
+  addNew() {
+    console.log('Add New Clicked');
+  }
 }
+
